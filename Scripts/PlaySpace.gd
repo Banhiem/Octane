33a827048aad3bee929e4652110b2dcf0188fa55
Daniel Medina <medinad9265@gmail.com>
2026-04-13 15:02:50 -0700

    cleaned up playspace script
    seperated out variables into distinct commented categories for clarity
    and removed unnecesary code

--- Scripts/PlaySpace.gd
+++ Scripts/PlaySpace.gd
@@ -1,46 +1,44 @@
 extends Node2D
 
-@onready var PlayerHand = load("res://Scripts/Label.gd")
-@onready var Cards = load("res://Scenes/cards.tscn")
-@onready var NewCard = Cards.instantiate()
-@onready var CardSelected = []
-@onready var DeckSize = PlayerHand.CardList.size()
-@onready var ViewPort = $BackGround/TableTop.size
-@onready var Card = load("res://Scenes/cards.tscn").instantiate()
-@onready var Focus = Card.get_node("Focus")
-@onready var t = 1
-@onready var P1Cards = []
-@onready var P1Down = []
-@onready var P2Down = []
-@onready var P3Down = []
-@onready var P4Down = []
-@onready var PileCards = []
-#@onready var CardSize=Vector2(130,200)
-#@onready var NewCard=Cards.instantiate()
+# --- Resources ---
+var Deck = preload("res://Scripts/Label.gd")
+var CardTemplate = preload("res://Scenes/cards.tscn")
 
+# --- Runtime state ---
+var DeckSize = null
+var ViewPort = null
+var Focus = null
+var CardSelected = []
+var P1Cards = []
+var P1Down = []
+var P2Down = []
+var P3Down = []
+var P4Down = []
+var PileCards = []
+var t = 1
+
+# --- Runtime nodes ---
+var new_card
+var card
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	ViewPort = $BackGround/TableTop.size
 	$BackGround.scale *= (ViewPort / $BackGround.texture.get_size()) * 0.75
 	$BackGround/TableTop.position = $BackGround.position
-	# print(NewCard.position)
-	#if(NewCard.position==$CardPile.position):
-	#NewCard.z_index*=0
-	#NewCard.z_as_relative=false
-	#NewCard.top_layer=true
-	#print($Cards.get_index())
+	DeckSize = Deck.CardList.size()
 
 
 func _input(event):
 	if Input.is_action_just_pressed("spacebar"):
 		CardSelected = randi_range(0, DeckSize - 1)
-		var NewCard = Cards.instantiate()
-		NewCard.Name = PlayerHand.CardList[CardSelected]
-		#NewCard.position=$P1Hand.position
+		var NewCard = CardTemplate.instantiate()
+		NewCard.Name = Deck.CardList[CardSelected]
+
 		$Cards.add_child(NewCard)
-		#$Cards.get_index()
 		$Cards.move_child(NewCard, 0)
-		PlayerHand.CardList.erase(PlayerHand.CardList[CardSelected])
+
+		Deck.CardList.erase(Deck.CardList[CardSelected])
 		DeckSize -= 1
 		if (DeckSize >= 68):
 			NewCard.position = $P1Hand.position
@@ -60,24 +58,3 @@ func _input(event):
 			NewCard.position = $P4DownCards.position
 			$P4DownCards.position += Vector2(0, 130)
 			NewCard.rotation_degrees = -90
-		#if(DeckSize<48):
-		#NewCard.position=$CardPile.position
-		#if(DeckSize<47):
-		#NewCard.position=$Discard.position
-		# print(PileCards)
-	#if (NewCard.position.y==1050):
-	#P1Cards.push_front(NewCard.Name)
-	#if (NewCard.position.y==850):
-	#P1Down.push_front(NewCard.Name)
-	#if (NewCard.position.x==600):
-	#P2Down.push_front(NewCard.Name)
-	#if (NewCard.position.y==300):
-	#P3Down.push_front(NewCard.Name)
-	#if (NewCard.position.x==1900):
-	#P4Down.push_front(NewCard.Name)
-	#if (NewCard.position==Vector2(500,500)):
-	#PileCards.push_front(NewCard.Name)
-	# print(NewCard.position)
-	#if Input.is_action_just_pressed("left mouse click"):
-
-	#func _process(delta):
